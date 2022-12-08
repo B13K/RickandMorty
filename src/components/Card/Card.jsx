2c4bda86c4aa719b4404dacdbd7118cdb80ffcd9
Jonathan <jonathan_3_7@hotmail.com>
2022-12-08 01:48:49 -0500

    HW React Redux

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,56 @@
 //import styled from 'styled-components'
+import React, {useEffect} from 'react';
 import styles from './Card.module.css'
 import { Link } from 'react-router-dom';
 
-export default function Card(props) {
+import { connect } from 'react-redux';
+//Import actions
+import {addFavorite, deleteFavorite } from '../../redux/actions.js'
+
+function Card(props) {
+
+   let [isFav, setIsFav] = React.useState(false)
 
    let detailId = props.id;
 
+   useEffect(() => {      
+      // props.myFavorites?.forEach((fav) => {
+      //    if (fav.id === props.id) {
+      //       setIsFav(true);
+      //    }
+      // });
+      for(let i = 0; i< props.myFavorites?.length; i++){
+         if(props.myFavorites[i].id === props.id){
+            setIsFav(true)
+         }
+      }
+   }, [props.myFavorites])
+
+
+   function handleFavorite(){
+      if(isFav){         
+         props.deleteFavorite(props.id)
+         setIsFav( isFav = false )
+      }
+      else{
+         props.addFavorite(props)
+         setIsFav( isFav = true )
+      }
+   }
+
 
    return (
+      
       <div className={styles.card}>
-         <div className={styles.buttonClass}>      
-            <button onClick={props.onClose}>X</button>
+         <div className={styles.buttonClass}>  
+         {
+            isFav ? (
+               <button onClick={handleFavorite}>❤️</button>
+            ) : (
+               <button onClick={handleFavorite}>🤍</button>
+            )
+         }    
+            <button onClick={props.onClose} className={styles.buttonClose}>X</button>
          </div>
          <div className={styles.imgClass}>
             <img  src={props.image} alt="" />
@@ -25,3 +65,21 @@ export default function Card(props) {
       </div>
    );
 }
+
+
+export function mapStateToProps(state){
+   return {
+      myFavorites: state?.myFavorites
+   }
+}
+
+export function mapDispatchToProps(dispatch){
+   return {
+      addFavorite: (character) => dispatch(addFavorite(character)),
+      deleteFavorite: (id) => dispatch(deleteFavorite(id))
+      }
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Card);
---
+++ src/redux/reducer.js
@@ -0,0 +1,20 @@
+import { ADD_FAVORITE, DELETE_FAVORITE } from "./types"
+
+const initialState = {
+    myFavorites: []
+}
+
+export default function rootReducer(state=initialState, action){
+    switch(action.type){
+        case ADD_FAVORITE:
+            return ({
+                ...state,
+                myFavorites: [...state.myFavorites, action.payload]
+            });
+        case DELETE_FAVORITE:
+            return({
+                ...state,
+                myFavorites: [...state.myFavorites.filter(e => e.id !== action.payload)]
+            })
+    }
+}
